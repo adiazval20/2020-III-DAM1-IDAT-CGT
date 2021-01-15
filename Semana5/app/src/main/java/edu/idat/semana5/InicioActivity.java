69dca591c813c5f65fe254c356dc7d9b9b0bf090
Andy <adiazval20@gmail.com>
2021-01-14 20:59:54 -0500

    0

---
+++ Semana5/app/src/main/java/edu/idat/semana5/InicioActivity.java
@@ -0,0 +1,14 @@
+package edu.idat.semana5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
