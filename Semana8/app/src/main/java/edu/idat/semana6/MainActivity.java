8cea4b7a947838d410fdf20a69aec80fe805c899
Andy <adiazval20@gmail.com>
2021-02-04 19:00:21 -0500

    0

---
+++ Semana8/app/src/main/java/edu/idat/semana6/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.idat.semana6;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.idat.semana6.adapter.PostAdapter;
+import edu.idat.semana6.entity.Post;
+import edu.idat.semana6.repository.PostRepository;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lsvPosts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lsvPosts = findViewById(R.id.lsvPosts);
+
+        PostAdapter adapter = new PostAdapter(this, R.layout.item_post, PostRepository.list());
+        lsvPosts.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.optCrearPost:
+                Intent intent = new Intent(this, PostDataActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+}
